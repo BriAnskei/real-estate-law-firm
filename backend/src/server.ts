0746883607cb42870d910834024d38fe5318f931
Brian <brianebrahim7@gmail.com>
2025-12-16 23:16:55 +0800

    env for port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,7 +19,7 @@ import notificationRoute from "./route/notification.router.js";
 import dashboardRoute from "./route/dashboard.route.js";
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cookieParser());
