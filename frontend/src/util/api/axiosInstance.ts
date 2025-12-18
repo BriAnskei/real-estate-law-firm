9357c2f3574b1af7619a04dc61ac3da2b52ebeb4
Brian <brianebrahim7@gmail.com>
2025-12-18 20:22:51 +0800

    fixed configuration

--- frontend/src/util/api/axiosInstance.ts
+++ frontend/src/util/api/axiosInstance.ts
@@ -4,7 +4,7 @@ import { AppDispatch, RootState } from "../../store/store";
 import { refreshTokens } from "../../store/Slice/authSlice";
 
 const api = axios.create({
-  baseURL: import.meta.env.BASE_URL || "http://localhost:4000",
+  baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:4000",
   withCredentials: true,
 });
 
